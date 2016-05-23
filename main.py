790c280396639f3a8619d662d26b4343de15d2e3
Daniel Eliassen <danieleliassen@users.noreply.github.com>
2016-05-23 14:56:01 +0200

    Update main.py
    Removed comment and variable instead of looping over results from method call.

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ import count_kmers
 def main():
     configuration = SparkConf().setAppName("1000-genomes Project")
     spark_context = SparkContext(conf=configuration)
-    for item in count_kmers.main(spark_context).collect():
+    kmers_count = count_kmers.main(spark_context).collect() 
+    for item in kmers_count:
         word, count = item
-        sys.stdout.write("{0}, {1}\n".format(word, count))#'%s, %s\n' % word, count)
+        sys.stdout.write("{0}, {1}\n".format(word, count))
     return
 
 if __name__ == '__main__':
